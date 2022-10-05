26b4cb0d45cdc5df4ecccb7c7285ad7f499ed5cd
wander217 <Trinhthinh2171999@gmail.com>
2022-10-05 14:31:45 +0700

    initial

--- utils/weight_init.py
+++ utils/weight_init.py
@@ -1,15 +1,15 @@
 import torch.nn as nn
 
 
-def init_weight(module):
-    if isinstance(module, nn.Conv2d):
-        module.weight.data.normal_(0., 0.02)
-    elif isinstance(module, nn.BatchNorm2d):
-        module.weight.data.normal_(1., 0.02)
-        module.bias.data.fill_(0.)
-    elif isinstance(module, nn.LayerNorm):
-        module.weight.data.normal_(1., 0.02)
-        module.bias.data.fill_(0.)
-    elif isinstance(module, nn.Linear):
-        module.weight.data.fill_(1.)
-        module.bias.data.fill_(0.)
+def init_weight(m):
+    if isinstance(m, nn.Conv2d):
+        m.weight.data.normal_(0., 0.02)
+    elif isinstance(m, nn.BatchNorm2d):
+        m.weight.data.normal_(1., 0.02)
+        m.bias.data.fill_(0.)
+    elif isinstance(m, nn.LayerNorm):
+        m.weight.data.normal_(1., 0.02)
+        m.bias.data.fill_(0.)
+    elif isinstance(m, nn.Linear):
+        m.weight.data.fill_(1.)
+        m.bias.data.fill_(0.)
