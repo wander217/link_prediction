ab16f70abced517d2d4f199d9b09b1aeaadc23e6
wander217 <Trinhthinh2171999@gmail.com>
2022-10-10 00:42:54 +0700

    initial

--- measure/measure.py
+++ measure/measure.py
@@ -5,8 +5,7 @@ from torch import Tensor
 class Measure:
 
     def __call__(self, pred: Tensor, target: Tensor):
-        _, pred = pred.max(1)
-        pred_mat: np.ndarray = pred.detach().cpu().numpy()
+        pred_mat: np.ndarray = (pred > 0.5).int().detach().cpu().numpy()
         target_mat: np.ndarray = target.detach().cpu().numpy()
         n_correct: int = (pred_mat == target_mat).sum()
         return n_correct
